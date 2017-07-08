805b1708805b8000e2133f4fd80280479e84c933
Dan Niyazov <daniel.niyazov@stonybrook.edu>
2017-07-07 22:34:42 -0400

    add properties

---
+++ src/cc/CodeCheckProp.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc;
+
+/**
+ *
+ * @author danniyazov
+ */
+public enum CodeCheckProp {
+    PREV_BUTTON_TEXT,
+    NEXT_BUTTON_TEXT,
+    HOME_BUTTON_TEXT,
+    NEW_BUTTON_TEXT,
+    LOAD_BUTTON_TEXT,
+    RENAME_BUTTON_TEXT,
+    ABOUT_BUTTON_TEXT,
+    REMOVE_BUTTON_TEXT,
+    UNZIP_BUTTON_TEXT,
+    EXTRACT_BUTTON_TEXT,
+    EXTRACT_CODE_BUTTON_TEXT,
+    CHECK_CODE_BUTTON_TEXT,
+    RESULTS_BUTTON_TEXT,
+    STEP_ONE_TITLE,
+    STEP_TWO_TITLE,
+    STEP_THREE_TITLE,
+    STEP_FOUR_TITLE,
+    STEP_FIVE_TITLE,
+    STEP_ONE_DESCRIP,
+    STEP_TWO_DESCRIP,
+    STEP_THREE_DESCRIP,
+    STEP_FOUR_DESCRIP,
+    STEP_FIVE_DESCRIP,
+    STEP_ONE_PROGRESS,
+    STEP_TWO_PROGRESS,
+    STEP_THREE_PROGRESS,
+    STEP_FOUR_PROGRESS,
+    STEP_FIVE_PROGRESS,
+    STEP_ONE_LIST_TITLE,
+    STEP_TWO_LIST_TITLE,
+    STEP_THREE_LIST_TITLE,
+    STEP_FOUR_LIST_TITLE,
+    STEP_FIVE_LIST_TITLE,
+    
+    PREV_BUTTON_ICON,
+    NEXT_BUTTON_ICON,
+    HOME_BUTTON_ICON,
+    RENAME_BUTTON_ICON,
+    ABOUT_BUTTON_ICON,
+    REMOVE_BUTTON_ICON,
+    UNZIP_BUTTON_ICON,
+    EXTRACT_CODE_BUTTON_ICON,
+    CHECK_CODE_BUTTON_ICON,
+    RESULTS_BUTTON_ICON
+
+    
+    
+}
