ac62b4f44f454a402b8d6fa1cb712dd4affeaf6c
Dan Niyazov <appleidname@yahoo.com>
2017-07-10 23:48:32 -0400

    foolproofing

--- src/cc/startup/CodeCheckWelcomeViewController.java
+++ src/cc/startup/CodeCheckWelcomeViewController.java
@@ -1,4 +1,5 @@
 /*
+ * Author: Dan Niyazov 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
