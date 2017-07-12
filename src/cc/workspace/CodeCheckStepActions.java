7fed9d8b7c97fa449f36039b275ae030e6d48f1e
Dan Niyazov <daniel.niyazov@stonybrook.edu>
2017-07-12 02:07:44 -0400

    step action button lookup

---
+++ src/cc/workspace/CodeCheckStepActions.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc.workspace;
+
+/**
+ *
+ * @author danniyazov
+ */
+public enum CodeCheckStepActions {
+    EXTRACT_SUBMISSIONS,
+    RENAME_SUBMISSIONS,
+    UNZIP_SUBMISSIONS,
+    EXTRACT_CODE,
+    CODE_CHECK,
+    VIEW_RESULTS
+}
